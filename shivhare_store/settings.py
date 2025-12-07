20ca6a22a623c9e91a2afa71d052ce6f5ffb8889
unayusual <24f3005110@ds.study.iitm.ac.in>
2025-12-08 02:29:41 +0530

    EMERGENCY FIX: Enable DEBUG mode to force CSS loading

--- shivhare_store/settings.py
+++ shivhare_store/settings.py
@@ -25,9 +25,10 @@ SECRET_KEY = 'django-insecure-%4f4#^%d2sfl%@-8g&pju((=0h^0wq3lxk-1a$yv^%-3b)5ov&
 import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# ENABLED TEMPORARILY TO FIX CSS LOADING ISSUES
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = ['https://*.vercel.app']
 
